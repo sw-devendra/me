f4a3d977bce9059c4e6a26489ea47678c30c7499
Devendra <sw.devendra@gmail.com>
2018-11-01 05:46:38 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,5 +24,35 @@ function init() {
        stopDegree = 90
        rotate()
     }
+    if ( event.key == 'r' ) {
+        event.preventDefault();
+        direction = 'Y'
+        stopDegree = 270
+        rotate()
+    }  
+    if ( event.key == 'b' ) {
+        event.preventDefault();
+        direction = 'Y'
+        stopDegree = 180
+        rotate()
+    }  
+    if ( event.key == 'f' ) {
+        event.preventDefault();
+        direction = 'Y'
+        stopDegree = 360
+        rotate()
+    }  
+    if ( event.key == 't' ) {
+        event.preventDefault();
+        direction = 'X'
+        stopDegree = 90
+        rotate()
+    }            
+    if ( event.key == 'b' ) {
+        event.preventDefault();
+        direction = 'X'
+        stopDegree = 270
+        rotate()
+    }                 
   });
 }
