8fe1cb21b2e8423b86e349e6708829ccfc767cae
Devendra <sw.devendra@gmail.com>
2018-11-01 05:22:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+var $elie = $(".cube"), degree = 0, timer;
+var stopDegree;
+var direction
+
+function rotate() {
+        if (stopDegree == degree) {
+          degree = 0
+          return;
+        }
+  
+        $elie.css({ WebkitTransform: 'rotate' + direction + '(' + degree + 'deg)'});  
+        $elie.css({ '-moz-transform': 'rotate' + direction + '(' + degree + 'deg)'});                      
+        timer = setTimeout(function() {
+            ++degree; rotate();
+        },5);
+    }
+
 function init() {
+  
+
+  rotate();
+  
   $( ".cube" ).keypress(function( event ) {
     if ( event.which == 'l' ) {
        event.preventDefault();
-       $(".cube").addClass('leftview')
+       direction = 'Y'
+       stopDegree = 90
+       rotate()
     }
   });
 }
